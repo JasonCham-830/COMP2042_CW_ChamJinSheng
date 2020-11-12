b237173473668f66a9055bcadd5a8915b47b3ab8
Cham Jin Sheng <jasoncham0830@hotmail.com>
2020-11-13 01:24:19 +0800

    Refractor Files

---
+++ src/FroggerApp/Actor/Digit.java
@@ -0,0 +1,21 @@
+package FroggerApp.Actor;
+
+import javafx.scene.image.Image;
+
+public class Digit extends Actor{
+	int dim;
+	Image im1;
+	@Override
+	public void act(long now) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public Digit(int n, int dim, int x, int y) {
+		im1 = new Image("file:src/FroggerApp/Images_File/"+n+".png", dim, dim, true, true);
+		setImage(im1);
+		setX(x);
+		setY(y);
+	}
+	
+}
